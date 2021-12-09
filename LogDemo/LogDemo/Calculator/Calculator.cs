870a7acb1e28b4cf3f38e2aa1a0646944c4c37df
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2021-12-09 09:28:13 -0600

    Add the add method

--- LogDemo/LogDemo/Calculator/Calculator.cs
+++ LogDemo/LogDemo/Calculator/Calculator.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace LogDemo.Calculator
 {
-    class Calculator
+    public class Calculator
     {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
     }
 }
